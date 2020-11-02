90d09c8a3a5c58480c402545b0996db9c192d038
Abdullah Meda <am2024@hw.ac.uk>
2020-11-02 21:37:36 +0400

    Adding basic chat function

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+
+const app = express();
+const clientPath = `${__dirname}/../client`;
+
+console.log(`serving static from ${clientPath}`);
+
+app.use(express.static(clientPath));
+
+const server = http.createServer(app);
+const io = socketio(server)
+
+io.on('connection', (sock) => {
+  sock.emit('message', 'You are connected')
+
+  sock.on('message', (text) => io.emit('message', text))
+})
+
+server.on('error', (err) => {
+  console.error('Server error:', err);
+});
+
+server.listen(8080, () => {
+  console.log('server started on 8080');
+});
