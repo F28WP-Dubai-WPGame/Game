7fe0cf639239a8cee4785125b796ff50bf5fd91e
Abdullah Meda <am2024@hw.ac.uk>
2020-11-13 20:33:46 +0400

    Error

--- app.js
+++ app.js
@@ -1,13 +1,43 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const { log } = require('console');
+var express = require('express');
+var app = express();
+var serv = require('http').Server(app);
 
-const app = express();
-app.use(express.static("public"));
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/client/index.html');
+});
+app.use('/client', express.static(__dirname + '/client'));
 
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
+serv.listen(3000, function () {
+  console.log("Server started on http://localhost:3000/");
 });
 
-app.listen(3000, function () {
-  console.log("Server is running on port 3000");
+var SOCKET_LIST = {};
+
+var io = require('socket.io')(serv, {});
+io.sockets.on('connection', function (socket) {
+
+  socket.id = Math.random();
+  socket.currentIndex = 0;
+  SOCKET_LIST[socket.id] = socket;
+
+  socket.on('disconnect', function () {
+  });
 });
+
+setInterval(function () {
+  var pack = [];
+
+  for (var i in SOCKET_LIST) {
+    var socket = SOCKET_LIST[i];
+    pack.push({
+      currentIndex: socket.currentIndex
+    })
+  }
+
+  for (var i in SOCKET_LIST) {
+    var socket = SOCKET_LIST[i];
+    socket.emit('newPosition', pack)
+  }
+
+}, 1000 / 25)
