718a728a90dfa9535de897f00ef8396bdb595ed2
msadiq10 <mb200@hw.ac.uk>
2020-11-15 10:22:35 +0400

    Update chat.js

--- client/chat.js
+++ client/chat.js
@@ -1,4 +1,9 @@
+var chatText = document.getElementById('chat-text');
+var chatInput = document.getElementById('chat-input');
+var chatForm = document.getElementById('chat-form');
+
 var today = new Date;
+var socket = io();
 document.getElementById('date').innerHTML = today.getHours() + ":" + today.getMinutes();
 
 socket.on('addToChat', function (data) {
